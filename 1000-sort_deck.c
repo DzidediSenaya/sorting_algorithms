5d2b42385ad0b9d87eae504078009c31f604c00a
Dzidedi Senaya <dzidedisenaya@gmail.com>
2023-07-24 22:47:34 +0000

    Update 1000-sort_deck.c

--- 1000-sort_deck.c
+++ 1000-sort_deck.c
@@ -12,90 +12,75 @@ int get_value_index(const char *value);
  * swap_nodes - Swaps two nodes in a doubly linked list
  * @node1: First node to swap
  * @node2: Second node to swap
- *
- * This function swaps the positions of two nodes in a doubly linked list.
  */
 void swap_nodes(deck_node_t *node1, deck_node_t *node2)
 {
-    if (node1->prev)
-        node1->prev->next = node2;
-    if (node2->next)
-        node2->next->prev = node1;
+if (node1->prev)
+node1->prev->next = node2;
+if (node2->next)
+node2->next->prev = node1;
 
-    node1->next = node2->next;
-    node2->prev = node1->prev;
-    node1->prev = node2;
-    node2->next = node1;
+node1->next = node2->next;
+node2->prev = node1->prev;
+node1->prev = node2;
+node2->next = node1;
 }
 
 /**
  * list_length - Returns the length of a doubly linked list
  * @list: Pointer to the head of the list
- *
  * Return: Length of the list
- *
- * This function calculates the length of a doubly linked list.
  */
 size_t list_length(const deck_node_t *list)
 {
-    size_t len = 0;
-    while (list)
-    {
-        len++;
-        list = list->next;
-    }
-    return len;
+size_t len = 0;
+while (list)
+{
+len++;
+list = list->next;
+}
+return len;
 }
 
 /**
  * compare_cards - Helper function to compare two cards
  * @card1: Pointer to the first card
  * @card2: Pointer to the second card
- *
  * Return: Positive number if card1 > card2,
  *         Negative number if card1 < card2,
  *         Zero if card1 == card2
- *
- * This function is used as a comparison function for qsort to compare two cards
- * based on their values and kinds. It returns a positive number if card1 should
- * appear after card2 in the sorted order, a negative number if card1 should
- * appear before card2, and zero if they have equal values and kinds.
  */
 int compare_cards(const void *card1, const void *card2)
 {
-    const card_t *c1 = (const card_t *)card1;
-    const card_t *c2 = (const card_t *)card2;
-    int value_diff = get_value_index(c1->value) - get_value_index(c2->value);
+const card_t *c1 = (const card_t *)card1;
+const card_t *c2 = (const card_t *)card2;
+int value_diff = get_value_index(c1->value) - get_value_index(c2->value);
 
-    if (value_diff != 0)
-        return value_diff;
+if (value_diff != 0)
+return value_diff;
 
-    return c1->kind - c2->kind;
+return c1->kind - c2->kind;
 }
 
 /**
  * get_value_index - Helper function to get the index of a card value
  * @value: Value of the card
- *
  * Return: Index of the card value
- *
- * This function maps the card values to their corresponding indices
- * in the sorting order.
  */
 int get_value_index(const char *value)
 {
-int i;
+int (i);
 static const char *values[] = {
-        "Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"
-    };
+"Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"
+};
 
-    for (i = 0; i < 13; i++)
-    {
-        if (strcmp(value, values[i]) == 0)
-            return i;
-    }
+for (i = 0; i < 13; i++)
+{
+if (strcmp(value, values[i]) == 0)
+return (i);
+}
 
-    return -1;
+return (-1);
 }
 
 /**
@@ -108,35 +93,31 @@ static const char *values[] = {
  */
 void sort_deck(deck_node_t **deck)
 {
-    size_t len;
-    size_t i;
-    deck_node_t *current;
-    const card_t **cards;
+size_t len;
+size_t i;
+deck_node_t *current;
+const card_t **cards;
 
-    if (deck == NULL || *deck == NULL)
-        return;
+if (deck == NULL || *deck == NULL)
+return;
 
-    len = list_length(*deck);
-    cards = malloc(len * sizeof(const card_t *));
-    if (!cards)
-        return;
+len = list_length(*deck);
+cards = malloc(len * sizeof(const card_t *));
+if (!cards)
+return;
 
-    current = *deck;
-    for (i = 0; i < len; i++)
-    {
-        cards[i] = current->card;
-        current = current->next;
-    }
-
-    qsort(cards, len, sizeof(card_t *), compare_cards);
-
-    current = *deck;
-    for (i = 0; i < len; i++)
-    {
-        current->card = cards[i];
-        current = current->next;
-    }
-
-    free(cards);
+current = *deck;
+for (i = 0; i < len; i++)
+{
+cards[i] = current->card;
+current = current->next;
 }
-
+qsort(cards, len, sizeof(card_t *), compare_cards);
+current = *deck;
+for (i = 0; i < len; i++)
+{
+current->card = cards[i];
+current = current->next;
+}
+free(cards);
+}
