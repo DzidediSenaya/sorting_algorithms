9008b9dc81ec0c322958ff20fe681bf6c52bb44b
Dzidedi Senaya <dzidedisenaya@gmail.com>
2023-07-25 00:03:55 +0000

    Update 100-shell_sort.c

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -4,40 +4,39 @@
 
 /**
  * shell_sort - Sorts an array of integers in ascending order
- *              using the Shell sort algorithm with Knuth sequence
+ * using the Shell sort algorithm with Knuth sequence
  * @array: The array to be sorted
  * @size: Number of elements in the array
  */
 void shell_sort(int *array, size_t size)
 {
-    size_t gap = 1, i, j;
-    int temp;
+size_t gap = 1, i, j;
+int temp;
 
-    if (array == NULL || size < 2)
-        return;
+if (array == NULL || size < 2)
+return;
 
-    while (gap < size / 3)
-        gap = gap * 3 + 1;
+while (gap < size / 3)
+gap = gap * 3 + 1;
 
-    while (gap > 0)
-    {
-        for (i = gap; i < size; i++)
-        {
-            temp = array[i];
-            j = i;
+while (gap > 0)
+{
+for (i = gap; i < size; i++)
+{
+temp = array[i];
+j = i;
 
-            while (j >= gap && array[j - gap] > temp)
-            {
-                array[j] = array[j - gap];
-                j -= gap;
-            }
+while (j >= gap && array[j - gap] > temp)
+{
+array[j] = array[j - gap];
+j -= gap;
+}
 
-            array[j] = temp;
-        }
+array[j] = temp;
+}
+print_array(array, size);
 
-        print_array(array, size);
-
-        gap = (gap - 1) / 3;
-    }
+gap = (gap - 1) / 3;
+}
 }
 
