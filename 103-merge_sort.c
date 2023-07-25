5b51a6834520c35d2901bcff4aae22dfe2b66713
Dzidedi Senaya <dzidedisenaya@gmail.com>
2023-07-25 00:18:14 +0000

    Update 103-merge_sort.c

--- 103-merge_sort.c
+++ 103-merge_sort.c
@@ -12,22 +12,21 @@
  */
 void merge(int *array, int *left, size_t left_size, int *right, size_t right_size)
 {
-    size_t i, j, k;
-    i = j = k = 0;
+size_t i, j, k;
+i = j = k = 0;
 
-    while (i < left_size && j < right_size)
-    {
-        if (left[i] <= right[j])
-            array[k++] = left[i++];
-        else
-            array[k++] = right[j++];
-    }
+while (i < left_size && j < right_size)
+{
+if (left[i] <= right[j])
+array[k++] = left[i++];
+else
+array[k++] = right[j++];
+}
+while (i < left_size)
+array[k++] = left[i++];
 
-    while (i < left_size)
-        array[k++] = left[i++];
-
-    while (j < right_size)
-        array[k++] = right[j++];
+while (j < right_size)
+array[k++] = right[j++];
 }
 
 /**
@@ -41,36 +40,35 @@ void merge_sort(int *array, size_t size)
 size_t mid, left_size, right_size, i;
 int *temp_array, *left, *right;
 
-   if (array == NULL || size < 2)
-        return;
+if (array == NULL || size < 2)
+return;
 
-    mid = size / 2;
-    left_size = mid;
-    right_size = size - mid;
+mid = size / 2;
+left_size = mid;
+right_size = size - mid;
+left = array;
+right = array + mid;
 
-    left = array;
-    right = array + mid;
+merge_sort(left, left_size);
+merge_sort(right, right_size);
 
-    merge_sort(left, left_size);
-    merge_sort(right, right_size);
+printf("Merging...\n[left]: ");
+print_array(left, left_size);
+printf("[right]: ");
+print_array(right, right_size);
 
-    printf("Merging...\n[left]: ");
-    print_array(left, left_size);
-    printf("[right]: ");
-    print_array(right, right_size);
+temp_array = malloc(size * sizeof(int));
+if (temp_array == NULL)
+return;
 
-    temp_array = malloc(size * sizeof(int));
-    if (temp_array == NULL)
-        return;
+merge(temp_array, left, left_size, right, right_size);
 
-    merge(temp_array, left, left_size, right, right_size);
+for (i = 0; i < size; i++)
+array[i] = temp_array[i];
 
-    for (i = 0; i < size; i++)
-        array[i] = temp_array[i];
+printf("[Done]: ");
+print_array(array, size);
 
-    printf("[Done]: ");
-    print_array(array, size);
-
-    free(temp_array);
+free(temp_array);
 }
 
