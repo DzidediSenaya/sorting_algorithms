ffbedddc3ad806deb8dd619b2c9c3de245122a3d
Dzidedi Senaya <dzidedisenaya@gmail.com>
2023-07-25 00:00:29 +0000

    betty 1-insertion_sort_list.c

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -13,38 +13,37 @@ void insertion_sort_list(listint_t **list)
 listint_t *current = (*list)->next;    
 
 if (list == NULL || *list == NULL || (*list)->next == NULL)
-        return;
+return;
 
-    while (current != NULL)
-    {
-        listint_t *insertion_point = current->prev;
-        listint_t *next_node = current->next;
+while (current != NULL)
+{
+listint_t *insertion_point = current->prev;
+listint_t *next_node = current->next;
 
-        while (insertion_point != NULL && insertion_point->n > current->n)
-        {
-            /* Swap nodes */
-            insertion_point->next = current->next;
-            if (current->next != NULL)
-                current->next->prev = insertion_point;
+while (insertion_point != NULL && insertion_point->n > current->n)
+{
+/* Swap nodes */
+insertion_point->next = current->next;
+if (current->next != NULL)
+current->next->prev = insertion_point;
 
-            current->next = insertion_point;
-            current->prev = insertion_point->prev;
+current->next = insertion_point;
+current->prev = insertion_point->prev;
 
-            if (insertion_point->prev != NULL)
-                insertion_point->prev->next = current;
-            insertion_point->prev = current;
+if (insertion_point->prev != NULL)
+insertion_point->prev->next = current;
+insertion_point->prev = current;
 
-            if (current->prev == NULL)
-                *list = current;
+if (current->prev == NULL)
+*list = current;
 
-            /* Move to the previous node for the next iteration */
-            insertion_point = current->prev;
-            /* Print the list after each swap */
-            print_list(*list);
-        }
-
-        /* Move to the next unsorted node */
-        current = next_node;
-    }
+/* Move to the previous node for the next iteration */
+insertion_point = current->prev;
+/* Print the list after each swap */
+print_list(*list);
+}
+/* Move to the next unsorted node */
+current = next_node;
+}
 }
 
