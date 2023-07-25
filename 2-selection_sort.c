899cb3c97e3476c320c9ebebad5590b9c3caa277
Dzidedi Senaya <dzidedisenaya@gmail.com>
2023-07-25 00:40:02 +0000

    Update 2-selection_sort.c

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -14,28 +14,27 @@ size_t x, y, min_index;
 int temp;    
 
 if (array == NULL || size < 2)
-        return;
+return;
 
-    for (x = 0; x < size - 1; x++)
-    {
-        min_index = x;
+for (x = 0; x < size - 1; x++)
+{
+min_index = x;
 
-        for (y = x + 1; y < size; y++)
-        {
-            if (array[y] < array[min_index])
-                min_index = y;
-        }
+for (y = x + 1; y < size; y++)
+{
+if (array[y] < array[min_index])
+min_index = y;
+}
+if (min_index != x)
+{
+/* Swap elements */
+temp = array[x];
+array[x] = array[min_index];
+array[min_index] = temp;
 
-        if (min_index != x)
-        {
-            /* Swap elements */
-            temp = array[x];
-            array[x] = array[min_index];
-            array[min_index] = temp;
-
-            /* Print the array after each swap */
-            print_array(array, size);
-        }
-    }
+/* Print the array after each swap */
+print_array(array, size);
+}
+}
 }
 
