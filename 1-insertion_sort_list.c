f1fdfb4e2567b8aa0f1c1577248f9a0f1bbbd5c4
Dzidedi Senaya <dzidedisenaya@gmail.com>
2023-07-25 22:10:41 +0000

    Update 1-insertion_sort_list.c

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -8,11 +8,6 @@
  * ascending order using the Insertion sort algorithm
  * @list: Double pointer to the head of the doubly linked list
  */
-/**
- * insertion_sort_list - Sorts a doubly linked list of integers in
- * ascending order using the Insertion sort algorithm
- * @list: Double pointer to the head of the doubly linked list
- */
 void insertion_sort_list(listint_t **list)
 {
 listint_t *current = (*list)->next;
@@ -25,9 +20,9 @@ while (current != NULL)
 listint_t *insertion_point = current->prev;
 listint_t *next_node = current->next;
 
-while (insertion_point != NULL &&
-	insertion_point->n > current->n)
+while (insertion_point != NULL && insertion_point->n > current->n)
 {
+/* Swap nodes */
 insertion_point->next = current->next;
 if (current->next != NULL)
 current->next->prev = insertion_point;
@@ -42,9 +37,14 @@ insertion_point->prev = current;
 if (current->prev == NULL)
 *list = current;
 
+if (insertion_point != NULL)
+insertion_point->next = current;
+/* Move to the previous node for the next iteration */
 insertion_point = current->prev;
+/* Print the list after each swap */
 print_list(*list);
 }
+/* Move to the next unsorted node */
 current = next_node;
 }
 }
